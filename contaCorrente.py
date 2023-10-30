2af140e2a1ce33b8222ee9d5260cefbfdeefb6f0
Gabriel Otaviano <gabriel.oc1@hotmail.com>
2023-10-30 20:50:49 -0300

    add get transfer and set

--- contaCorrente.py
+++ contaCorrente.py
@@ -1,18 +1,36 @@
 class Conta():
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo algo...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     pass
 
     def extrato(self):
-        print("Saldo de {} do Titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do Titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
